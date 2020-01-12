e67955677b751f43b81b8c694ea6f375d31338cd
Sofus Albertsen <soa@praqma.com>
2020-01-12 17:38:41 +0100

    Improving ci part of the pipeline Adding ability to run the files locally

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,2 +1,4 @@
-#! /bin/bash
-docker build -f app/Dockerfile -t micronaut-app app/
+#!/bin/bash
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
